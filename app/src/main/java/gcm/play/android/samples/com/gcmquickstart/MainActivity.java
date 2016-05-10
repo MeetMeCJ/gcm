78326328744adfa4209dd6b6b7ca47c2979ce949
Jaime Molina <jaimedurcal@gmail.com>
2016-05-10 17:49:11 +0200

    prueba de colaboracion

--- app/src/main/java/gcm/play/android/samples/com/gcmquickstart/MainActivity.java
+++ app/src/main/java/gcm/play/android/samples/com/gcmquickstart/MainActivity.java
@@ -31,8 +31,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -40,15 +38,9 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-        //if (id == R.id.action_graph) {
-       //     Intent i = new Intent(this, Graph.class);
-       //     startActivity(i);
-       //     return true;
-       // }
 
         return super.onOptionsItemSelected(item);
     }
